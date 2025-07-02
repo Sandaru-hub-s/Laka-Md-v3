e17ff8e228be7ae1d02329c621f8374a000433e1
Sandaru-hub-s <Sandarusamod1234567@gmail.com>
2025-07-02 19:29:37 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,5 +4,5 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-𝐋ᴀᴋᴀ-𝐌ᴅ=FkRVwACY#pA84nncG87paUjwNupxbjSkLMyAXy-my2HegY_P1VZg
+𝐋ᴀᴋᴀ-𝐌ᴅ=ctolnCgT#mQvCmtz9A0_rtb86PLHmUprzmigPVgmTq6mPh7484so
 };
