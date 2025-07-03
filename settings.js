537a868eb8192827a6806b24783cf66c47df4eef
Sandaru-hub-s <Sandarusamod1234567@gmail.com>
2025-07-03 20:01:23 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,6 +4,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "𝐋ᴀᴋᴀ-𝐌ᴅ=40RkXKhQ#2ur7M1DhoIlKdwN1Dh7qInyIelFQOGdk9Ue4KKz7xXc",
+SESSION_ID: process.env.SESSION_ID || "𝐋ᴀᴋᴀ-𝐌ᴅ=tlBh2LgA#xW-1B2l3EE_LMKyBSCZ0mgzMy6xfwO6VBYtDg7dvuYQ",
 POSTGRESQL_URL: process.env.POSTGRESQL_URL === undefined ? 'postgresql://postgres:lakamd@db.ndduyrkebubwlbwiljee.supabase.co:5432/postgres' : process.env.POSTGRESQL_URL
 };
